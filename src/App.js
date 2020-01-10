a57e57ab4f2bde56782253f92730f410c0008f58
Crutishnyk <andrey.shcherbakov05@gmail.com>
2020-01-11 00:29:41 +0200

    Remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -5,14 +5,11 @@ import { BrowserRouter, Route, Switch  } from 'react-router-dom';
 import home from './components/home';
 
 const ENV_SETTINGS = require('./env')();
-// import NotFound from "./components/NotFound";
 
 function AppRoutes() {
   return (
     <Switch>
-      {/* <Route exact path="*" component={NotFound} /> */}
       <Route exact path={ENV_SETTINGS.path} component={home} title="VideoCutTool" />
-      {/* <Route component={NotFound} /> */}
     </Switch>
   );
 }
