d0f2d92684cf0a1ece797d2efc895fb25121315f
Anish Aggarwal <anish17122000@gmail.com>
2020-02-02 17:34:52 +0530

    fixed not found page
    Change-Id: I55862204ef71f69bc88c79fe2259f7cb8365699a

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,12 +4,13 @@ import { Button } from 'antd';
 import "../App.css";
 import "antd/dist/antd.css";
 
+const ENV_SETTINGS = require('../env')();
 class NotFound extends Component {
 
     render() {
         return (
             <div>
-                <Button href="https://tools.wmflabs.org/video-cut-tool-front-end/">Go to Main page</Button>
+                <Button href={ENV_SETTINGS.home_page}>Go to Main page</Button>
                 <img alt="404 Page Not Found" src="https://illustatus.herokuapp.com/?title=Oops,%20Page%20not%20found&fill=%234f86ed"/>
             </div>
         );
--- src/env.js
+++ src/env.js
@@ -3,6 +3,8 @@ module.exports = function(){
         case "production":
             return {
                 path: "/video-cut-tool/",
+                home_page:"https://tools.wmflabs.org/video-cut-tool",
+                not_found_path:"/",
                 backend_url: "https://tools.wmflabs.org/video-cut-tool-back-end",
                 socket_io_path: "/video-cut-tool-back-end/socket.io"
             }
@@ -10,6 +12,8 @@ module.exports = function(){
         default:
             return {
                 path: "/",
+                home_page:"/",
+                not_found_path:"/",
                 backend_url: "http://localhost:4000",
                 socket_io_path: "/video-cut-tool-back-end/socket.io"
             }
