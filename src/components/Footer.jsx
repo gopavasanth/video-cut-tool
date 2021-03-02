5c2088cba78d13e4e9717823301850c71a16ee63
gopavasanth <gopavasanth1999@gmail.com>
2021-03-03 00:10:38 +0530

    Updated Package.json and Year in the footer
    Added Author, Licence, Keywords, Description and Bugs details in package.json
    
    Change-Id: Idbf071cac9627fdf78d10f22ef004158bb6bf68d

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = Layout.Footer;
 class VideoCutToolFooter extends React.Component {
     render () {
       return <Footer style={{ textAlign: "center", marginTop: "auto" }}>
-        © 2019-20&nbsp;
+        © 2019-21&nbsp;
         <Message id="footer-authors" placeholders={[
           <a href="https://www.mediawiki.org/wiki/User:Gopavasanth">
             <span>Gopa Vasanth</span>
