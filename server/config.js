2ee7a538342b7b1179dbfbab66ca735742feeaaa
ramushetty <ummadishettyramu@gmail.com>
2022-05-24 16:10:03 +0530

    switch statement for selecting env settings

--- server/config.js
+++ server/config.js
@@ -1,27 +1,27 @@
 let config
 
+switch (process.env.NODE_ENV) {
+	case 'production':
+		config = {
+			DB_CONNECTION_URL: 'mongodb://localhost:27017/video-cut-tool',
+			BACKEND_URL: 'https://beta-videocuttool.wmflabs.org/api/',
+			PORT: 4000,
 
-if (process.env.NODE_ENV == 'production') {
-	config = {
-		DB_CONNECTION_URL: 'mongodb://localhost:27017/video-cut-tool',
-		BACKEND_URL: 'https://beta-videocuttool.wmflabs.org/api/',
-		PORT: 4000,
+			// Ouath 2
+			CLIENT_ID: '',
+			CLIENT_SECRET: ''
+		};
 
-		// Ouath 2
-		CLIENT_ID: '',
-		CLIENT_SECRET: ''
-	};
-}
-else {
-	config = {
-		DB_CONNECTION_URL: 'mongodb://videocuttool-mongo:27017/video-cut-tool',
-		BACKEND_URL: 'https://beta-videocuttool.wmflabs.org/api/',
-		PORT: 4000,
+	default:
+		config = {
+			DB_CONNECTION_URL: 'mongodb://videocuttool-mongo:27017/video-cut-tool',
+			BACKEND_URL: 'https://beta-videocuttool.wmflabs.org/api/',
+			PORT: 4000,
 
-		// Ouath 2
-		CLIENT_ID: '',
-		CLIENT_SECRET: ''
-	};
+			// Ouath 2
+			CLIENT_ID: '',
+			CLIENT_SECRET: ''
+		};
 }
 
 export default config;
