4ec8daf09aa2d217c2f994e6f0cc1fc3dca0f42e
ramushetty <ummadishettyramu@gmail.com>
2022-05-24 16:06:29 +0530

    environmental specific configuration added

--- server/config.js
+++ server/config.js
@@ -1,10 +1,27 @@
-const config = {
-	DB_CONNECTION_URL: 'mongodb://videocuttool-mongo:27017/video-cut-tool',
-	BACKEND_URL: 'https://beta-videocuttool.wmflabs.org/api/',
-	PORT: 4000,
+let config
 
-	// Ouath 2
-	CLIENT_ID: '',
-	CLIENT_SECRET: ''
-};
+
+if (process.env.NODE_ENV == 'production') {
+	config = {
+		DB_CONNECTION_URL: 'mongodb://localhost:27017/video-cut-tool',
+		BACKEND_URL: 'https://beta-videocuttool.wmflabs.org/api/',
+		PORT: 4000,
+
+		// Ouath 2
+		CLIENT_ID: '',
+		CLIENT_SECRET: ''
+	};
+}
+else {
+	config = {
+		DB_CONNECTION_URL: 'mongodb://videocuttool-mongo:27017/video-cut-tool',
+		BACKEND_URL: 'https://beta-videocuttool.wmflabs.org/api/',
+		PORT: 4000,
+
+		// Ouath 2
+		CLIENT_ID: '',
+		CLIENT_SECRET: ''
+	};
+}
+
 export default config;
